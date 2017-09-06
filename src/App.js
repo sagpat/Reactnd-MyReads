357135e195d6c25d21074d720293a7f6af88a685
Sagar Patil <saagar.1647@gmail.com>
2017-09-06 23:28:55 +0530

    fix: Make changes for 404 Page
    Added all the components under Swich tag for React Rouer 4.2.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import MyReads from './MyReads'
 import SearchBooks from './SearchBooks'
-
+import NotFound from './NotFound'
 
 class BooksApp extends React.Component {
 
@@ -65,20 +65,24 @@ class BooksApp extends React.Component {
 
   render() {
     return (
-      <div className="app">
-         <Route exact path="/" render={() =>(
-            <MyReads
-              books={this.state.books}
-              onAdjustBookShelf={this.adjustBooksShelf} />
-        )}/>
+          <div className="app">
+            <Switch>
+              <Route exact path="/" render={() =>(
+                   <MyReads
+                     books={this.state.books}
+                     onAdjustBookShelf={this.adjustBooksShelf} />
+              )}/>
 
-        <Route path="/search" render={()=>(
-          <SearchBooks
-              onAdjustFoundBooksShelf={this.adjustFoundBooksShelf}
-              onAdjustBookShelf={this.adjustBooksShelf}
-           />
-        )}/>
-      </div>
+              <Route path="/search" render={()=>(
+                <SearchBooks
+                    onAdjustFoundBooksShelf={this.adjustFoundBooksShelf}
+                    onAdjustBookShelf={this.adjustBooksShelf}
+                />
+              )}/>
+
+              <Route component={NotFound} />
+            </Switch>
+          </div>
     )
   }
 }
