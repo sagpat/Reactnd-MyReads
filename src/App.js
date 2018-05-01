abcfa889370a3ed3f5f442670e56b7dc242a19d4
Sagar Patil <saagar.1647@gmail.com>
2018-05-01 21:10:03 +0530

    change for master

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import SearchBooks from './SearchBooks'
 import NotFound from './NotFound'
 
 class BooksApp extends React.Component {
-
   state = {
     books:[]
   }
-
   componentDidMount(){
     this.getAllBooks()
   }
@@ -22,8 +20,8 @@ class BooksApp extends React.Component {
     })
   }
 
+// This is a change for develop branch!!
   findInBookShelf = (books, bookToFind) => (books.find((book) => (book.id === bookToFind.id)))
-
   adjustBooksShelf = (bookInTransition, shelf) => {
     BooksAPI.update(bookInTransition, shelf).then(() =>{
       this.setState(state => {
